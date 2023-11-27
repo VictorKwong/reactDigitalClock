3d3e54b5d71dc0efbc9cf276f48c7b47e6b329d9
victorkaiwong <victorkaihong@gmail.com>
2023-11-26 22:29:23 -0500

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Clock from './Clock';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className='text-center Small shadow container p-4'>
-      <Clock />
+    <div>
+      <Navbar />
+      <div className='text-center Small shadow container p-4'>
+        <Clock />
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand p-2" href="#">VrClock</a>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">GitHub</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            </ul>
+        </div>
+        </nav>
+    </div>
+  );
+}
+
+export default Navbar;
